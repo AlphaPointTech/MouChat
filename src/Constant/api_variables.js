bdca73e5a1e6ac6886bf2396d39a0670d991993f
root <root@ip-172-26-1-132.ec2.internal>
2024-10-15 11:42:13 +0000

    Server fixes

--- src/Constant/api_variables.js
+++ src/Constant/api_variables.js
@@ -1,6 +1,6 @@
 
 // first varialbe for local host dev. Second one for when pushing to production :)
-export const BASE_URL = 'http://localhost:8080'
+export const BASE_URL = 'http://44.215.20.161:8014'
 // export const BASE_URL = 'https://medadashboardbackend-bmgkh6hzambqaxcf.southafricanorth-01.azurewebsites.net'
 
 export const gpt_url = '/myopiaAI'
